abf5669b1b0a3cdab578c2be9c3377d36cb19f62
itmajing <itmajing@163.com>
2020-05-05 00:19:15 +0800

    feat(all): determine browser is chromium

---
+++ public/index.js
@@ -0,0 +1,11 @@
+(() => {
+  // determine browser is chromium
+  const agent = window.navigator.userAgent;
+  const isChromium = agent.indexOf('Chrome') > -1;
+  if (!isChromium) {
+    window.location.href = 'browser/index.html';
+  }
+
+  // calculate the file loading progress
+  // TODO
+})();
