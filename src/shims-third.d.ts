1cc882a921a4b8a01a9e8b6749b44312c4b20ee1
itmajing <itmajing@163.com>
2020-05-04 22:46:53 +0800

    feat(all): add editorconfig

--- src/shims-third.d.ts
+++ src/shims-third.d.ts
@@ -1,13 +1,3 @@
-declare module 'spark-md5' {
-  class SparkMd5 {
-    appendBinary(contents:string):void;
-    end():string;
-    destroy():void;
-  }
-
-  export default SparkMd5;
-}
-
 declare module 'vue-particles' {
   let VueParticles: any;
   export default VueParticles;
