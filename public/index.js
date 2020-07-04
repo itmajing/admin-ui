962efa7bb8c3af85f10a3fabcf1b6ebeee713a89
itmajing <itmajing@163.com>
2020-07-05 02:02:16 +0800

    feat(location): fix index.js syntax error

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
-(() => {
+;(function() {
   // determine browser is chromium
-  const agent = window.navigator.userAgent;
-  const isChromium = agent.indexOf('Chrome') > -1;
+  const agent = window.navigator.userAgent
+  const isChromium = agent.indexOf('Chrome') > -1
   if (!isChromium) {
-    window.location.href = 'browser/index.html';
+    window.location.href = 'browser/index.html'
   }
 
   // calculate the file loading progress
   // TODO
-})();
+})()
