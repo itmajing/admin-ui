5c23a4cdfdda4781862e62d00287853f36b38742
itmajing <itmajing@163.com>
2020-08-16 20:09:44 +0800

    feat(location): add locale provider

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
-    <router-view />
+    <a-locale-provider :locale="locale">
+      <router-view />
+    </a-locale-provider>
   </div>
 </template>
+
+<script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+
+export default {
+  data() {
+    return {
+      locale: zhCN,
+    }
+  },
+}
+</script>
 
 <style lang="less">
 html,
