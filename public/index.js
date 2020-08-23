585020b07ffb4026d677a87ac0bcc718878ce69e
itmajing <itmajing@163.com>
2020-08-23 14:37:28 +0800

    feat(location): determine platform is pc

--- public/index.js
+++ public/index.js
@@ -1,11 +1,26 @@
+/* eslint-disable prefer-const */
 ;(function() {
-  // determine browser is chromium
-  const agent = window.navigator.userAgent
-  const isChromium = agent.indexOf('Chrome') > -1
-  if (!isChromium) {
-    window.location.href = 'browser/index.html'
-  }
+  if (window) {
+    let userAgent = window.navigator.userAgent
+    window.console.log(userAgent)
 
-  // calculate the file loading progress
-  // TODO
+    // determine platform is pc
+    let mobileAgents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPad', 'iPod']
+    for (let i = 0; i < mobileAgents.length; i++) {
+      if (userAgent.indexOf(mobileAgents[i]) > -1) {
+        window.location.href = '/platform'
+        break
+      }
+    }
+
+    // determine browser is chromium
+    const isChromium = userAgent.indexOf('Chrome') > -1
+    if (!isChromium) {
+      window.location.href = '/browser'
+    }
+
+    // calculate the file loading progress
+    // TODO
+  }
 })()
+/* eslint-disable prefer-const */
